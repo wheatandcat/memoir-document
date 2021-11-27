613145adac830a1cb763e0e3f64c413fdfcc719b
wheatandcat <wheatandcat@gmail.com>
2021-11-27 21:45:21 +0900

    init

---
+++ slide_001/theme/layouts/cover.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { handleBackground } from "../layoutHelper";
+
+const props = defineProps({
+  background: {
+    // random image from a curated Unsplash collection by Anthony
+    default: "",
+  },
+});
+
+const style = computed(() => handleBackground(props.background, true));
+</script>
+
+<template>
+  <div class="slidev-layout cover text-center" :style="style">
+    <div class="my-auto w-full">
+      <slot />
+    </div>
+  </div>
+</template>
